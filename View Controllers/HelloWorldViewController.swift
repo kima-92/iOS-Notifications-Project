c861dfec15860acf72d8eff169e676f92885ad24
Wilma <kima_92@fastmail.com>
2019-11-14 18:19:10 -0500

    App now saves state when running in the background

--- View Controllers/HelloWorldViewController.swift
+++ View Controllers/HelloWorldViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class HelloWorldViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,5 +33,54 @@ class HelloWorldViewController: UIViewController {
     }
     @objc private func changeToGreen() {
         view.backgroundColor = .green
+    }
+    var color: String?
+    
+    override func encodeRestorableState(with coder: NSCoder) {
+        defer { super.encodeRestorableState(with: coder) }
+        
+        
+        switch view.backgroundColor {
+        case UIColor.red:
+            color = "red"
+        case UIColor.blue:
+            color = "blue"
+        case UIColor.green:
+            color = "green"
+        default:
+            color = "clear"
+        }
+        
+        var colorData: Data?
+        
+        do {
+            colorData = try PropertyListEncoder().encode(color)
+        } catch {
+            NSLog("Error encoding color \(error)")
+        }
+        
+        guard let currentColor = colorData else { return }
+        
+        coder.encode(currentColor, forKey: "currentColor")
+    }
+    
+    override func decodeRestorableState(with coder: NSCoder) {
+        defer { super.decodeRestorableState(with: coder) }
+        
+        guard let colorData = coder.decodeObject(forKey: "currentColor") as? Data else { return }
+        
+        self.color = try? PropertyListDecoder().decode(String.self, from: colorData)
+        
+        switch color {
+        case "red":
+            view.backgroundColor = .red
+        case "blue":
+            view.backgroundColor = .blue
+        case "green":
+            view.backgroundColor = .green
+        default:
+            color = "clear"
+        }
+        
     }
 }
