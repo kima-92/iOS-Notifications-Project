77ad5eba6575db2a12ca2dff1db5fdbc5e4b2832
Wilma <kima_92@fastmail.com>
2019-11-14 17:15:06 -0500

    Added Notifications.name extension

---
+++ Helpers/Notifications.swift
@@ -0,0 +1,15 @@
+//
+//  Notifications.swift
+//  Notifications Project
+//
+//  Created by macbook on 11/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var red = Notification.Name("red")
+    static var blue = Notification.Name("blue")
+    static var green = Notification.Name("green")
+}
