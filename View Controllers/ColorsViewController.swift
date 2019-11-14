aeaa97385c9e628c4896308bb6ad4b21d2922878
Wilma <kima_92@fastmail.com>
2019-11-14 17:14:42 -0500

    Added ColorsViewController with IBActions

---
+++ View Controllers/ColorsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ColorsViewController.swift
+//  Notifications Project
+//
+//  Created by macbook on 11/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController {
+    
+    // Color Buttons
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func redButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func blueButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
