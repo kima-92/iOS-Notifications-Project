a1fbd811be1a4c07689f9d2f8ec5bb5bb28f226f
Wilma <kima_92@fastmail.com>
2019-11-14 17:17:29 -0500

    Added String Keys for the colors

---
+++ Helpers/StringKeys.swift
@@ -0,0 +1,15 @@
+//
+//  StringKeys.swift
+//  Notifications Project
+//
+//  Created by macbook on 11/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static var red = "red"
+    static var blue = "blue"
+    static var green = "green"
+}
